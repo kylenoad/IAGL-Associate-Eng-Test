be40f3027bb79b32d83371c8f218856b4d4804be
kylenoad <email@kylenoad.co.uk>
2025-07-03 11:30:13 +0100

    Add service layer test: should get todos from repository

--- backend/test/service/todo.test.js
+++ backend/test/service/todo.test.js
@@ -1,20 +1,33 @@
-
-
-describe('TODO Service', () => {
-  it('should be able to get todos from repository', async () => {
+describe("TODO Service", () => {
+  it("should be able to get todos from repository", async () => {
     const expected = {
       todos: [
         {
-          task: "This is a task to be done"
-        }
-      ]
+          task: "This is a task to be done",
+        },
+      ],
     };
     const todoRepository = {
-      getTodos: async () => Promise.resolve(expected)
+      getTodos: async () => Promise.resolve(expected),
     };
 
-    const todoService = require('../../src/service/todo')(todoRepository);
+    const todoService = require("../../src/service/todo")(todoRepository);
     const actual = await todoService.getTodos();
+    expect(actual).toEqual(expected);
+  });
+
+  it("should be able to add a todo to repository", async () => {
+    const newTodo = { task: "New task" };
+    const expected = { task: "New task" };
+
+    const todoRepository = {
+      addTodo: async (todo) => Promise.resolve(todo),
+      getTodos: async () => Promise.resolve({ todos: [expected] }),
+    };
+
+    const todoService = require("../../src/service/todo")(todoRepository);
+
+    const actual = await todoService.addTodo(newTodo);
     expect(actual).toEqual(expected);
   });
 });
